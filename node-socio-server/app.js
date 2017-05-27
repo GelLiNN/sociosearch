6da0a2558b5675c98a8da958dc61ee2546dabf09
Kellan Nealy <gellinn@uw.edu>
2017-05-27 16:29:24 -0700

    Added Bare bones for login functionality:
    Added LokiJS to the project and initialized database
    Added views for login and register
    Added bcrypt for encryption
    Added passport for logging in functionality
    Added Flash messages
    Added Express validator
    Updated app.js

--- node-socio-server/app.js
+++ node-socio-server/app.js
@@ -4,8 +4,22 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var session = require('express-session');
+var flash = require('connect-flash');
+var passport = require('passport');
+var localStrategy = require('passport-local'),Strategy;
 
-var index = require('./routes/index');
+// Initialize LokiJS Database
+var loki = require('lokijs');
+var db = new loki('sociosearch.db');
+// Add a collection to the database
+var users = db.addCollection('users');
+// Add some documents to the collection
+users.insert({ email : 'chikn42@gmail.com', password: '890p890p' });
+console.log(users.find({ 'email': 'chikn42@gmail.com' }));
+
+var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
@@ -15,32 +29,70 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'assets/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport Init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
+    }
+}));
+
+// Connect Flash and global vars for flash messages
+app.use(flash());
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+/* catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});*/
 
 module.exports = app;
--- node-socio-server/routes/users.js
+++ node-socio-server/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* Register */
+router.get('/register', function(req, res) {
+    res.render('register');
+});
+
+/* Login */
+router.get('/login', function(req, res) {
+    res.render('login');
 });
 
 module.exports = router;
