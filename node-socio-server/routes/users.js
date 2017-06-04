fac19e32ea6971eb41b2849825ba7d1abe219925
Kellan Nealy <gellinn@uw.edu>
2017-06-03 17:53:17 -0700

    Added registration with bootstrap:
      -Incorporated bootstrap into register view at users/register
      -Added server side form validation with POST on routes/users.js
      -Registration form has annoying huge padding, will fix later

--- node-socio-server/routes/users.js
+++ node-socio-server/routes/users.js
@@ -10,6 +10,29 @@ router.get('/about', function(req, res) {
 router.get('/register', function(req, res) {
     res.render('register');
 });
+router.post('/register', function(req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    // Validation
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+    var errors = req.validationErrors();
+    if (errors) {
+        res.render('register', {
+            errors:errors
+        });
+    } else {
+        console.log('successful');
+    }
+    console.log(name);
+});
 
 /* Login */
 router.get('/login', function(req, res) {
