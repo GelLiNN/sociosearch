d18b8e296869a44568b5d17b82cf1725d22991da
Kellan Nealy <gellinn@uw.edu>
2017-06-13 01:07:16 -0700

    Style Changes and Additions:
    -Added universal custom footer for the site
    -Updates to navbar with working nav active functionality
    -Coherent styling in general for the site
    -Search page style updates
    -New sociosearch.js script for my client side javascript needs

---
+++ node-socio-server/public/js-scripts/sociosearch.js
@@ -0,0 +1,10 @@
+/* Client-side javascript for SocioSearch */
+
+/* On document load update active nav element */
+$(document).ready(function () {
+    var url = window.location;
+    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+    $('ul.nav a').filter(function() {
+         return this.href == url;
+    }).parent().addClass('active');
+});
