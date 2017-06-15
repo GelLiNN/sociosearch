6e8a477b4cafeae262b10e9a7f4164219a67969f
Kellan Nealy <gellinn@uw.edu>
2017-06-15 13:42:47 -0700

    Trying to get the recipe right for this in travis. Worked successfully locally many times.

--- tests/testRunner.sh
+++ tests/testRunner.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Travis should already be inside node-socio-server here
-./node-socio-server/bin/start -test &
+# Travis should be in the root directory here
+cd node-socio-server
+./bin/start -test &
 sleep 2
 # Run each test file with given executable if needed
-./node-socio-server/node_modules/phantomjs/bin/phantomjs tests/console-errors-test.js
+./node_modules/phantomjs/bin/phantomjs ../tests/console-errors-test.js
