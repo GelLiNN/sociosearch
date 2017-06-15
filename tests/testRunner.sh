198e470821ddee110224a1358eb1c6984050d175
Kellan Nealy <gellinn@uw.edu>
2017-06-15 13:36:58 -0700

    Now that travis can use the testRunner, it needs correct filepaths

--- tests/testRunner.sh
+++ tests/testRunner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Travis should already be inside node-socio-server here
-./bin/start -test &
+./node-socio-server/bin/start -test &
 sleep 2
 # Run each test file with given executable if needed
-./node_modules/phantomjs/bin/phantomjs ../tests/console-errors-test.js
+./node-socio-server/node_modules/phantomjs/bin/phantomjs tests/console-errors-test.js
