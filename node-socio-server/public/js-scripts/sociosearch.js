de685d76203b640a4b5bf9a6f91230b25605e10d
gellinn <gellinn@uw.edu>
2017-07-25 20:31:47 -0700

    Added CAPTCHA to registration:
    Now the register form is not enabled until you successfully
    Bend over and get SLAPPED.  Take that user.  JK its good for you.
    
    hashtag protecting you from bots.

--- node-socio-server/public/js-scripts/sociosearch.js
+++ node-socio-server/public/js-scripts/sociosearch.js
@@ -162,3 +162,42 @@ function clearResults() {
     document.getElementById("trendsChart").innerHTML = "";
     document.getElementById("thumbnail-results").innerHTML = "";
 }
+
+/* CAPTCHA functionality for registration page */
+function Captcha(){
+    var alpha = new Array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+    var i;
+    for (i = 0; i < 6; i++){
+        var a = alpha[Math.floor(Math.random() * alpha.length)];
+        var b = alpha[Math.floor(Math.random() * alpha.length)];
+        var c = alpha[Math.floor(Math.random() * alpha.length)];
+        var d = alpha[Math.floor(Math.random() * alpha.length)];
+        var e = alpha[Math.floor(Math.random() * alpha.length)];
+        var f = alpha[Math.floor(Math.random() * alpha.length)];
+        var g = alpha[Math.floor(Math.random() * alpha.length)];
+    }
+    var code = a + ' ' + b + ' ' + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g;
+    document.getElementById("mainCaptcha").value = code
+}
+
+/* CAPTCHA validation for registration page */
+function ValidCaptcha(){
+    var string1 = captchaRemoveSpaces(document.getElementById('mainCaptcha').value);
+    var string2 = captchaRemoveSpaces(document.getElementById('txtInput').value);
+    var formElems = document.getElementsByClassName('form-control');
+    if (string1 == string2) {
+        // Enable registration inputs and submit button
+        for (var i = 0, len = formElems.length; i < len; i++) {
+            formElems[i].disabled = false;
+        }
+        document.getElementById('registerSubmit').disabled = false;
+    } else {
+        // Do something more elaborate for failed CAPTCHA
+    }
+    return string1 == string2;
+}
+
+/* CAPTCHA remove spaces helper */
+function captchaRemoveSpaces(string){
+    return string.split(' ').join('');
+}
